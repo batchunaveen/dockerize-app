c2f7b714853750d413f9662934316cd1345a9a3d
batchunaveen <batchunaveen6@gmail.com>
2018-07-24 00:07:06 -0400

    Initial commit

---
+++ src/main/java/com/naveen/applications/DockerizeAppApplication.java
@@ -0,0 +1,12 @@
+package com.naveen.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeAppApplication.class, args);
+	}
+}
